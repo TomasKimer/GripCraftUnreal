766641fca64df59304387fdb1057c595b46b3c7b
TomasKimer <tomas.kimer@gmail.com>
2020-10-08 09:53:18 +0200

    Center crosshair correctly to the center of screen
    Would except something more precise from an example project :)

--- Source/GripCraftUnreal/GripCraftUnrealHUD.cpp
+++ Source/GripCraftUnreal/GripCraftUnrealHUD.cpp
@@ -3,9 +3,9 @@
 #include "GripCraftUnrealHUD.h"
 #include "Engine/Canvas.h"
 #include "Engine/Texture2D.h"
-#include "TextureResource.h"
 #include "CanvasItem.h"
 #include "UObject/ConstructorHelpers.h"
+
 
 AGripCraftUnrealHUD::AGripCraftUnrealHUD()
 {
@@ -25,8 +25,8 @@ void AGripCraftUnrealHUD::DrawHUD()
 	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 
 	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	const FVector2D CrosshairDrawPosition( (Center.X),
-										   (Center.Y + 20.0f));
+	const FVector2D CrosshairDrawPosition( (Center.X - CrosshairTex->GetSizeX() * 0.5f),
+										   (Center.Y - CrosshairTex->GetSizeY() * 0.5f));
 
 	// draw the crosshair
 	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
